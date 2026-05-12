4517025002f7a9d86fafa1c279380dc1b6ae2cc2
rex <rex@linux.com>
2026-05-11 21:17:13 -0400

    Update

--- stage0/prerun.sh
+++ stage0/prerun.sh
@@ -6,5 +6,5 @@ if [ "$RELEASE" != "trixie" ]; then
 fi
 
 if [ ! -d "${ROOTFS_DIR}" ]; then
-	bootstrap ${RELEASE} "${ROOTFS_DIR}" http://raspbian.raspberrypi.com/raspbian/
+	bootstrap ${RELEASE} "${ROOTFS_DIR}" http://deb.debian.org/debian/
 fi
